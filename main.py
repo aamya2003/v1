2fd0118677ee28e016d4c470cffb68711a15ea0d
Ahmed Asaad <107361633+aamya2003@users.noreply.github.com>
2023-08-02 01:46:48 +0300

    update

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-  return "<b> hello</b>"
+  return request.base_url
 
 def run():
   app.run(host='0.0.0.0', port=8080)
@@ -19,23 +19,23 @@ def keep_alive():
 
 
 
-import telebot
+# import telebot
 
-bot = telebot.TeleBot("6336490086:AAEpQooiX8qpOQ-DY7ohRGSqcJ05KwwG2f4")
+# bot = telebot.TeleBot("6336490086:AAEpQooiX8qpOQ-DY7ohRGSqcJ05KwwG2f4")
 
-@bot.message_handler()
-def Myfunc(message):
-    bot.send_message(message.chat.id, "Hi, What's happend?")
+# @bot.message_handler()
+# def Myfunc(message):
+#     bot.send_message(message.chat.id, "Hi, What's happend?")
 
-@app.route('/bot_webhook', methods=['POST'])
-def bot_webhook():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode('utf-8'))])
+# @app.route('/bot_webhook', methods=['POST'])
+# def bot_webhook():
+#     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode('utf-8'))])
 
-    return 'OK'
+#     return 'OK'
 
 
-bot.remove_webhook()
+# bot.remove_webhook()
 
-bot.set_webhook("https://my-flask-heroku-535f0de7db25.herokuapp.com/bot_webhook")
+# bot.set_webhook("https://my-flask-heroku-535f0de7db25.herokuapp.com/bot_webhook")
 
 keep_alive()
