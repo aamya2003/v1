8670b3ed374dfa03eeff95556c8eefdca1c0a5de
Ahmed Asaad <107361633+aamya2003@users.noreply.github.com>
2023-08-04 14:00:17 +0300

    update

--- main.py
+++ main.py
@@ -1,40 +1,10 @@
-from flask import Flask, request
-import telebot
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Replace 'YOUR_TELEGRAM_BOT_TOKEN' with your actual bot token
-bot_token = '6336490086:AAEpQooiX8qpOQ-DY7ohRGSqcJ05KwwG2f4'
-bot = telebot.TeleBot(bot_token)
-
 @app.route('/')
 def index():
-    return "Hello, this is your Telegram bot!"
-
-
-print(request.base_url)
-
-
-# This is the endpoint where Telegram will send updates
-@app.route('/telegram-webhook', methods=['POST'])
-def webhook():
-    Name = "AHmed"
-    # Get the data from Telegram's POST request
-    json_string = request.get_data().decode('utf-8')
-    update = telebot.types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-    return ''
-
-
-# webhook_url = 'https://my-flask-heroku-535f0de7db25.herokuapp.com' + '/telegram-webhook'
-
-# # Set the webhook
-# bot.remove_webhook()
-# bot.set_webhook(url=webhook_url)
-
-
-
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    # Start Flask app
-    app.run(debug=True)
+    app.run()
