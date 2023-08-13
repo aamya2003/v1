f8af34e0f736bfe3d68252e64d47b4e74aa44ad3
Ahmed Asaad <107361633+aamya2003@users.noreply.github.com>
2023-08-14 00:51:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,39 @@
-from flask import Flask, render_template
+import telebot
 
-app = Flask(__name__)
+
+
+bot = telebot.TeleBot("6523294892:AAFp846TjoxfeAh50moRj4mEPV4whjNvgv4")
+bot.remove_webhook()
+
+@bot.message_handler()
+def Myfunc(message):
+  bot.send_message(message.chat.id, "Hi, What's happend?")
+
+
+
+
+
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
 
 @app.route('/')
-def index():
-    return render_template("inedex.html")
+def home():
+  return "<b> hello</b>"
 
-if __name__ == '__main__':
-    app.run(port=8080, debug=True)
+def run():
+  app.run(host='0.0.0.0', port=8080)
+
+
+def keep_alive():
+  t = Thread(target=run)
+  t.start()
+
+
+keep_alive()
+
+bot.infinity_polling(skip_pending=True)
+
+
